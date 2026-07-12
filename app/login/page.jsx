4ac52b354001f7a97bb5d802937af917dc2360ff
temihuss <tesnimhussien432@gmail.com>
2026-07-12 09:52:56 +0300

    feat: Built functional login form logic

---
+++ app/login/page.jsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useState } from "react";
+import { useRouter} from 'next/navigation';
+
+export default function LoginPage(){
+    const[email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const router = useRouter();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+    console.log("Login Attempted!");
+    console.log("Email captured:", email);
+    console.log("Password captured:", password);
+
+    alert("Check your browser console for data! Redirecting to Home...");
+    router.push('/');
+    };
+
+    return(
+        <div>
+            <h1>Login to Your Account</h1>
+
+            <form onSubmit={handleLogin}>
+                
+                <div>
+                    <label>Email: </label>
+                    <input type="email"
+                           value={email}
+                           onChange={(e) => setEmail(e.target.value)}
+                            required
+                            />
+                </div>
+
+                <div>
+                    <label>Password: </label>
+                    <input type="password"
+                           value={password}
+                           onChange={(e) => setPassword(e.target.value)}
+                           required />
+                </div>
+
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
